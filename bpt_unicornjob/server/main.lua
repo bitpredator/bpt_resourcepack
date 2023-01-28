6dc48e577dd931a7503ea59df4d89c6e478a59e9
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-01-29 00:21:54 +0100

    feat:(bpt_unicornjob new job unicorn)
    new work unicorn requires the use of ox_inventory(it is recommended to use the version I modified) and bpt_crafting

---
+++ bpt_unicornjob/server/main.lua
@@ -0,0 +1,23 @@
+TriggerEvent('esx_society:registerSociety', 'unicorn', 'Unicorn', 'society_unicorn', 'society_unicorn', 'society_unicorn', {
+    type = 'public'
+})
+ESX.RegisterServerCallback("bpt_unicornjob:SpawnVehicle", function(source, cb, model , props)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    if xPlayer.job.name ~= "unicorn" then 
+        print(('[^3WARNING^7] Player ^5%s^7 attempted to Exploit Vehicle Spawing!!'):format(source))
+        return
+    end
+    local SpawnPoint = vector3(Config.Zones.VehicleSpawnPoint.Pos.x, Config.Zones.VehicleSpawnPoint.Pos.y, Config.Zones.VehicleSpawnPoint.Pos.z)
+    ESX.OneSync.SpawnVehicle(joaat(model), SpawnPoint, Config.Zones.VehicleSpawnPoint.Heading, props, function(vehicle)
+        local vehicle = NetworkGetEntityFromNetworkId(vehicle)
+        while GetVehicleNumberPlateText(vehicle) ~= props.plate do
+            Wait(0)
+        end
+        TaskWarpPedIntoVehicle(GetPlayerPed(source), vehicle, -1)
+    end)
+    cb()
+end)
+if Config.MaxInService ~= -1 then
+    TriggerEvent('esx_service:activateService', 'unicorn', Config.MaxInService)
+end
