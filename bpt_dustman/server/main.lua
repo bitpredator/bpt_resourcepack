e077328d3f197e155f0d9a3e1573ed9e045b0bde
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-08-06 22:15:38 +0200

    chore: fixed unusable variables and functions

--- bpt_dustman/server/main.lua
+++ bpt_dustman/server/main.lua
@@ -2,20 +2,16 @@ TriggerEvent('esx_society:registerSociety', 'dustman', 'Dustman', 'society_dustm
     type = 'public'
 })
 
-if Config.MaxInService ~= -1 then
-    TriggerEvent('esx_service:activateService', 'dustman', Config.MaxInService)
-end
-
 ESX.RegisterServerCallback("bpt_dustmanjob:SpawnVehicle", function(source, cb, model , props)
     local xPlayer = ESX.GetPlayerFromId(source)
 
-    if xPlayer.job.name ~= "dustman" then 
+    if xPlayer.job.name ~= "dustman" then
         print(('[^3WARNING^7] Player ^5%s^7 attempted to Exploit Vehicle Spawing!!'):format(source))
         return
     end
     local SpawnPoint = vector3(Config.Zones.VehicleSpawnPoint.Pos.x, Config.Zones.VehicleSpawnPoint.Pos.y, Config.Zones.VehicleSpawnPoint.Pos.z)
-    ESX.OneSync.SpawnVehicle(joaat(model), SpawnPoint, Config.Zones.VehicleSpawnPoint.Heading, props, function(vehicle)
-        local vehicle = NetworkGetEntityFromNetworkId(vehicle)
+    ESX.OneSync.SpawnVehicle(joaat(model), SpawnPoint, Config.Zones.VehicleSpawnPoint.Heading, props, function()
+        local vehicle = NetworkGetEntityFromNetworkId()
         while GetVehicleNumberPlateText(vehicle) ~= props.plate do
             Wait(0)
         end
